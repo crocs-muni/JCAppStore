1da14d133210c3f12c89ee336442afa54b11dbe9
Jirka <horakj7@gmail.com>
2019-11-21 23:38:13 +0100

    win batch auto import key

--- installer/dependency/keyring.bat
+++ installer/dependency/keyring.bat
@@ -1,7 +1,8 @@
 @ECHO OFF
 echo "Include public key into GnuPG key ring"
 
-start gpg --import store.asc
-start gpg --
+gpg --import ./scripts/store.asc
+
+(echo trust && echo 5 && echo q)|gpg --command-fd 0 --expert --edit-key 7B9FE0F5
 
 echo "Done!"
