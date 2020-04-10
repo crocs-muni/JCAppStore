57c1ff5d527dde85a11abbb6b7c1ce1c560b21ba
Jirka <horakj7@gmail.com>
2020-04-10 20:24:09 +0200

    Update files for unix deployment, fix scripts

--- installer-unix/launcher.sh
+++ installer-unix/launcher.sh
@@ -3,31 +3,32 @@ VERSION=1.0
 DIR=./
 
 if ! cat jcappstore-do-not-ask.info ; then
-	if ! gpg --version > /dev/null 2&>1 ; then
-	   echo "You don't have GnuPG installed, the store will be unable to verify software integrity.\
+    if ! gpg --version > /dev/null 2&>1 ; then
+        echo "You don't have GnuPG installed, the store will be unable to verify software integrity.\
 	      this can be fixed anytime by GnuPG installation and JCAppStore key import."
-     echo "Next time, the application will start WITHOUT THIS NOTICE!"
-	else
-     if ! gpg --list-keys 7B9FE0F5 > /dev/null 2&>1 ; then
-		    echo "JCAppStore automatically verifies the software integrity for you. In order to do that" \
+        echo "Next time, the application will start WITHOUT THIS NOTICE!"
+    else
+        if ! gpg --list-keys 3D6FE2832EDFE9C9 > /dev/null 2&>1 ; then 
+            echo "JCAppStore automatically verifies the software integrity for you. In order to do that" \
 		      "we need to import our public key to your keyring and set the ultimate trust."
-		    echo "You don't have to import the key, then the verification will not work. This can be" \
+	    echo "You don't have to import the key, then the verification will not work. This can be" \
 		      "changed anytime."
-		    echo "Do you wish to import y/n?"
-		    read ANSWER
-		    while [ "$ANSWER" != "y" ] && [ "$ANSWER" != "n" ] ; do
-		       echo "Wrong answer. Try again:"
-		       read ANSWER
-		    done
-		    if [[ "$ANSWER" == "y" ]] ; then
-		       gpg --import ${DIRECTORY}/store.asc
-		       (echo 5 && echo y)|gpg --command-fd 0 --expert --edit-key 7B9FE0F5 trust
-		       echo "The key has been imported."
-		    else
-		       echo "The key can be imported anytime. Next time, the application will start WITHOUT THIS NOTICE!"
-		    fi
-     fi
-	fi
-  touch jcappstore-do-not-ask.info
+            echo "Do you wish to import y/n?"
+	    read ANSWER
+            while [ "$ANSWER" != "y" ] && [ "$ANSWER" != "n" ] ; do
+                echo "Wrong answer. Try again:"
+		read ANSWER
+	    done
+            if [[ "$ANSWER" == "y" ]] ; then
+                gpg --import ${DIRECTORY}/store.asc
+		(echo 5 && echo y)|gpg --command-fd 0 --expert --edit-key 3D6FE2832EDFE9C9 trust
+		echo "The key has been imported."
+            else
+		echo "The key can be imported anytime. Next time, the application will start WITHOUT THIS NOTICE!"
+            fi
+        fi
+    fi
+    touch jcappstore-do-not-ask.info
 fi
-java -jar ${DIR}/JCAppStore-${VERSION}.jar
+cd ${DIR}
+java -jar ./JCAppStore-${VERSION}.jar
